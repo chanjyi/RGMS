661fe2840c1d076ba015699555af7b793b7f3952
Wan Ning <wning825@gmail.com>
2025-12-21 02:49:35 +0800

    Sidebar changed to left

--- hod_page.php
+++ hod_page.php
@@ -17,17 +17,22 @@ if (!isset($_SESSION['email'] )) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>hod Page</title>
         <link rel="stylesheet" href="style.css">
-     <link rel="stylesheet" href="sidebar.css">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     </head>
 
     <body>
     <?php include 'sidebar.php'; ?>
-        <div class="container">
-            <h1>Welcome, hod!<span> <?= $_SESSION['name'];?></span></h1>
-            <p>This is the hod dashboard.</p>
+    <section class="home-section">
+            
+            <div class="welcome-text">
+                Welcome <?= $_SESSION['name']; ?>, 
+                <span>(<?= isset($_SESSION['role']) ? $_SESSION['role'] : 'Head of Department'; ?>)</span>
+            </div>
 
-        </div>
-        <script src="script.js"></script>
+            <hr style="border: 1px solid #3C5B6F; opacity: 0.3; margin-bottom: 20px;">
+
+            <p>This is the HOD dashboard.</p>
+            
+        </section>
     </body>
 </html>
--- reviewer_page.php
+++ reviewer_page.php
@@ -1,32 +1,37 @@
 <?php
 session_start();
-if (!isset($_SESSION['email'] )) {
-    // Redirect to login page if not logged in as admin
+if (!isset($_SESSION['email'])) {
     header('Location: index.php');
     exit();
 }
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>reviewer Page</title>
-        <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="sidebar.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reviewer Dashboard</title>
+    
+    <link rel="stylesheet" href="style.css">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
-    </head>
+</head>
+<body>
 
-    <body>
     <?php include 'sidebar.php'; ?>
-        <div class="container">
-            <h1>Welcome, reviewer!<span> <?= $_SESSION['name'];?></span></h1>
-            <p>This is the reviewer dashboard.</p>
+
+    <section class="home-section">
+        
+        <div class="welcome-text">
+            Welcome <?= $_SESSION['name']; ?>, 
+            <span>(<?= isset($_SESSION['role']) ? $_SESSION['role'] : 'Reviewer'; ?>)</span>
         </div>
-        <script src="script.js"></script>
-    </body>
+
+        <hr style="border: 1px solid #3C5B6F; opacity: 0.3; margin-bottom: 20px;">
+
+        <p>This is the reviewer dashboard.</p>
+        
+    </section>
+
+</body>
 </html>
